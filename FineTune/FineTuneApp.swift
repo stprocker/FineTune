c458970a40c8a5d828e49c26b23e29654b82aa2f
Ronit Singh <code.rs2001@gmail.com>
2025-12-31 23:08:15 -0500

    feat: wire up SettingsManager in app entry point

--- FineTune/FineTuneApp.swift
+++ FineTune/FineTuneApp.swift
@@ -3,12 +3,17 @@ import SwiftUI
 
 @main
 struct FineTuneApp: App {
-    @State private var audioEngine = AudioEngine()
+    @State private var audioEngine: AudioEngine
 
     var body: some Scene {
         MenuBarExtra("FineTune", systemImage: "slider.horizontal.3") {
             MenuBarPopupView(audioEngine: audioEngine)
         }
         .menuBarExtraStyle(.window)
+    }
+
+    init() {
+        let settings = SettingsManager()
+        _audioEngine = State(initialValue: AudioEngine(settingsManager: settings))
     }
 }
